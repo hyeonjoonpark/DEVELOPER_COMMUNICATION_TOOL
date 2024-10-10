335d05e9f3f96426d91fae423a32bdea661da8d9
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-10 10:52:10 +0900

    SET : project setting

---
+++ src/main/java/org/hyunjooon/communication_devtools/CommunicationDevtoolsApplication.java
@@ -0,0 +1,13 @@
+package org.hyunjooon.communication_devtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationDevtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunicationDevtoolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hyunjooon/communication_devtools/CommunicationDevtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyunjooon.communication_devtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationDevtoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
