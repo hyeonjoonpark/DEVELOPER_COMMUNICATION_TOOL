72a8b4418e7e805cb98c6becd1d485bb92440a84
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-11 23:42:13 +0900

    JpaAuditing 활성화

--- src/main/java/org/hyunjooon/communication_devtools/CommunicationDevtoolsApplication.java
+++ src/main/java/org/hyunjooon/communication_devtools/CommunicationDevtoolsApplication.java
@@ -2,8 +2,12 @@ package org.hyunjooon.communication_devtools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories
 public class CommunicationDevtoolsApplication {
 
     public static void main(String[] args) {
