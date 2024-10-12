c6abd6ae06ffa73bccea46c0d5f96fe38fa8e32d
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-12 11:50:01 +0900

    FEAT(handler): Signal Server
    Signal 서버를 위한 Handler 파일을 작성하였습니다

---
+++ src/main/java/org/hyunjooon/communication_devtools/global/handler/SignalingHandler.java
@@ -0,0 +1,25 @@
+package org.hyunjooon.communication_devtools.global.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class SignalingHandler extends TextWebSocketHandler {
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+}
