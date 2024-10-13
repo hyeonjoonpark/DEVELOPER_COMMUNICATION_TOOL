c073e836cad2f204c8547c26ee1a5b3d52b70a69
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-13 20:50:59 +0900

    Create WebConfig

---
+++ src/main/java/org/hyunjooon/communication_devtools/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.hyunjooon.communication_devtools.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .maxAge(1500);
+    }
+}
