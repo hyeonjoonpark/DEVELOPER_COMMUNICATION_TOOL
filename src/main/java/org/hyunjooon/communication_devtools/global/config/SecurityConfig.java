7584b5edd9ba5bf99a611b36bd8a095304b69120
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-13 19:59:16 +0900

    ADD : SecurityConfig 파일

---
+++ src/main/java/org/hyunjooon/communication_devtools/global/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package org.hyunjooon.communication_devtools.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    }
+}
