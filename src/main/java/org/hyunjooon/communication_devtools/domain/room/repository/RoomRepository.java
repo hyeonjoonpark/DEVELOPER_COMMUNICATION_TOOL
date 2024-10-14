b045885db43eb5dce1831d373b408fbe357fbacb
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-14 12:13:37 +0900

    :))

--- src/main/java/org/hyunjooon/communication_devtools/domain/room/repository/RoomRepository.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/room/repository/RoomRepository.java
@@ -1,4 +1,12 @@
 package org.hyunjooon.communication_devtools.domain.room.repository;
 
-public interface RoomRepository {
+import org.hyunjooon.communication_devtools.domain.room.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RoomRepository extends JpaRepository<ChatRoom, UUID> {
+    
 }
