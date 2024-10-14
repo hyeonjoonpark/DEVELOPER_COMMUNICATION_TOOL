24ba77dba4924cb59726bb96f596078b7d49d0f7
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-14 12:11:49 +0900

    FEAT(postComment): PostCommentRepository

--- src/main/java/org/hyunjooon/communication_devtools/domain/board_comment/repository/BoardCommentRepository.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/board_comment/repository/BoardCommentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardCommentRepository extends JpaRepository<BoardComment, Long> {
-    
+
 }
--- src/main/java/org/hyunjooon/communication_devtools/domain/post/repository/PostRepository.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/post/repository/PostRepository.java
@@ -1,4 +1,10 @@
 package org.hyunjooon.communication_devtools.domain.post.repository;
 
-public interface PostRepository {
+import org.hyunjooon.communication_devtools.domain.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
 }
