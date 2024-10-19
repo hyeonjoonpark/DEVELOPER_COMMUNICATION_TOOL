2f924e099f37c018002f93c3e750bb1ebdda9f62
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-19 22:00:15 +0900

    FIX(role): Role 정의 수정

--- src/main/java/org/hyunjooon/communication_devtools/domain/account/user/enums/Role.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/account/user/enums/Role.java
@@ -1,6 +1,22 @@
 package org.hyunjooon.communication_devtools.domain.account.user.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
 public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
+    ROLE_USER("일반 사용자"),
+    ROLE_ADMIN("관리자"),
+    ROLE_GUEST("게스트");
+
+    private final String name;
+
+    public static Role of(String name) {
+        return Arrays.stream(Role.values())
+                .filter(role -> role.getName().equals(name))
+                .findAny().orElse(ROLE_GUEST);
+    }
 }
