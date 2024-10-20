e85dee0cb6fe0d60a4a8eb3d273fc3abbcef2d41
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-20 17:28:05 +0900

    ADD PasswordEncoder
    PasswordEncoder Bean 등록

--- src/main/java/org/hyunjooon/communication_devtools/global/config/SecurityConfig.java
+++ src/main/java/org/hyunjooon/communication_devtools/global/config/SecurityConfig.java
@@ -5,13 +5,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
