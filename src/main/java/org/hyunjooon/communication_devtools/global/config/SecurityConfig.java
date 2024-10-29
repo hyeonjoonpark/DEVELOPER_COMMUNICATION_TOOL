ff717b71a5e60e2624ecc30d92d4cd6781c5ea07
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-30 00:30:23 +0900

    FIX: SecurityConfig

--- src/main/java/org/hyunjooon/communication_devtools/global/config/SecurityConfig.java
+++ src/main/java/org/hyunjooon/communication_devtools/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -36,10 +37,21 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers("/**").permitAll()
+                                .requestMatchers("/graphql/**", "/graphiql/**").permitAll()
                                 .anyRequest().permitAll()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer securityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(
+                        "/graphiql/**",
+                        "/graphql/**",
+                        "/favicon.ico"
+                );
     }
 }
