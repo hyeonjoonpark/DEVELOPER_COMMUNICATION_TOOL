3f7896f146880218374bb177099f16fafb88b98c
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-02 11:40:52 +0900

    .

--- src/main/java/org/hyunjooon/communication_devtools/global/exception/errorCode/ErrorCode.java
+++ src/main/java/org/hyunjooon/communication_devtools/global/exception/errorCode/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     USER_ALREADY_EXIST("이미 존재하는 사용자입니다", 409),
     INVALID_TOKEN("유효하지 않은 토큰입니다", 498),
     ACCESS_DENIED("접근이 거부되었습니다", 403),
+    WRONG_PASSWORD("잘못된 비밀번호입니다", 400),
     INTERNAL_SERVER_ERROR("서버에서 에러가 발생하였습니다", 500);
 
     private final String message;
