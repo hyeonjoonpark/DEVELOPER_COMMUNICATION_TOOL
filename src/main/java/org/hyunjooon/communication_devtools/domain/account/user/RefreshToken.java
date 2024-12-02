8a016bb5de4ca500a75bc051c50491e5c198b854
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-02 09:14:56 +0900

    FIX: RefreshToken 도메인 수정

--- src/main/java/org/hyunjooon/communication_devtools/domain/account/user/RefreshToken.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/account/user/RefreshToken.java
@@ -1,12 +1,20 @@
 package org.hyunjooon.communication_devtools.domain.account.user;
 
-import jakarta.persistence.Id;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
+@Getter
 @RedisHash(value = "refreshToken", timeToLive = 60) // TTL 일단 짧게 설정
 public class RefreshToken {
     @Id
     private Long id;
-    private String refreshToken;
-    private Long expiration;
+    private final String refreshToken;
+    private final Long expiration;
+
+    public RefreshToken(Long id, String refreshToken, Long expiration) {
+        this.id = id;
+        this.refreshToken = refreshToken;
+        this.expiration = expiration;
+    }
 }
