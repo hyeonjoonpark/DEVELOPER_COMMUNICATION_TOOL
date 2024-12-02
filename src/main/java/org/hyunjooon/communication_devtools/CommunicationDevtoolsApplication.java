1bc5e03bae6c5573f8f4ded463ec29a5bf5c4cde
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-02 12:11:39 +0900

    kafka gradle

--- src/main/java/org/hyunjooon/communication_devtools/CommunicationDevtoolsApplication.java
+++ src/main/java/org/hyunjooon/communication_devtools/CommunicationDevtoolsApplication.java
@@ -4,18 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableJpaRepositories
 @EnableCaching
-//@EnableKafkaStreams
 public class CommunicationDevtoolsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CommunicationDevtoolsApplication.class, args);
     }
-
 }
