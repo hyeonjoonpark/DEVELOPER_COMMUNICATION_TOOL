ac545c3f1bbb41dd2ff5a2221a0d480144f37a10
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-04 11:34:59 +0900

    FEAST: UserService
    signIn 로직 구현 완료

--- src/main/java/org/hyunjooon/communication_devtools/domain/account/user/service/UserService.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/account/user/service/UserService.java
@@ -4,12 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.hyunjooon.communication_devtools.domain.account.user.User;
 import org.hyunjooon.communication_devtools.domain.account.user.presentation.dto.request.SignInRequest;
 import org.hyunjooon.communication_devtools.domain.account.user.presentation.dto.request.SignUpRequest;
+import org.hyunjooon.communication_devtools.domain.account.user.presentation.dto.response.SignInResponse;
 import org.hyunjooon.communication_devtools.domain.account.user.repository.UserRepository;
 import org.hyunjooon.communication_devtools.domain.auth.details.CustomUserDetailService;
 import org.hyunjooon.communication_devtools.domain.auth.details.CustomUserDetails;
 import org.hyunjooon.communication_devtools.global.common.GlobalResponse;
 import org.hyunjooon.communication_devtools.global.exception.GlobalException;
 import org.hyunjooon.communication_devtools.global.exception.errorCode.ErrorCode;
+import org.hyunjooon.communication_devtools.global.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,11 +24,18 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final CustomUserDetailService customUserDetailService;
+    private final JwtUtil jwtUtil;
+
+    private static final Long ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60L;
+    private static final Long REFRESH_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 7L;
+
+    @Value("${jwt.secret}")
+    private String JWT_SECRET_KEY;
 
     public GlobalResponse<?> create(SignUpRequest request) {
 
         boolean isExist = userRepository.existsById(request.userId());
-        if(isExist) {
+        if (isExist) {
             throw new RuntimeException("이미 존재하는 사용자입니다");
         }
 
@@ -42,7 +52,7 @@ public class UserService {
                 .role(request.role())
                 .build();
         userRepository.save(user);
-        return GlobalResponse.success("성공적으로 회원가입 되었습니다", user, HttpStatus.CREATED);
+        return GlobalResponse.success("성공적으로 회원가입 되었습니다", user.getUserId(), HttpStatus.CREATED);
     }
 
     public GlobalResponse<?> signIn(SignInRequest signInRequest) throws GlobalException {
@@ -50,8 +60,15 @@ public class UserService {
         if (!passwordEncoder.matches(signInRequest.password(), user.getPassword())) {
             throw new GlobalException(ErrorCode.WRONG_PASSWORD);
         }
-
-        // create JWT
-        return null;
+        String accessToken = jwtUtil.createToken(signInRequest.email(), ACCESS_TOKEN_EXPIRATION_TIME, JWT_SECRET_KEY);
+        String refreshToken = jwtUtil.createToken(signInRequest.email(), REFRESH_TOKEN_EXPIRATION_TIME, JWT_SECRET_KEY);
+        return GlobalResponse.success("성공적으로 로그인되었습니다",
+                new SignInResponse(
+                        user.getUsername(),
+                        signInRequest.email(),
+                        accessToken,
+                        refreshToken
+                ), HttpStatus.OK
+        );
     }
 }
