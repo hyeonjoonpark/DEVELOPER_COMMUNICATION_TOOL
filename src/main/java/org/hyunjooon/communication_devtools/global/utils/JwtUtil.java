ca9efed6f840c6b6f365eb9dd80bbe6001c36fdb
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-04 11:33:49 +0900

    FEAT: Create JwtUtil
    Token 생성 로직 추가

---
+++ src/main/java/org/hyunjooon/communication_devtools/global/utils/JwtUtil.java
@@ -0,0 +1,20 @@
+package org.hyunjooon.communication_devtools.global.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    public String createToken(String email, Long expiration, String JWT_SECRET) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(SignatureAlgorithm.HS256, JWT_SECRET)
+                .compact();
+    }
+}
