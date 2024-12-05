bfe6f3877c975f45ac85148f0fdbcaf50c4ebf69
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-05 10:52:50 +0900

    .

---
+++ src/main/java/org/hyunjooon/communication_devtools/domain/auth/RefreshToken.java
@@ -0,0 +1,18 @@
+package org.hyunjooon.communication_devtools.domain.auth;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@RedisHash(value = "refreshToken", timeToLive = 60)
+public class RefreshToken {
+    @Id
+    private String userId;
+    private final String refreshToken;
+
+    public RefreshToken(String userId, String refreshToken) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+    }
+}
