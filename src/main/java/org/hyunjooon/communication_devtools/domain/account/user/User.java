985e0437341222c29b190c1f0f06c58f62805eed
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-05 16:19:57 +0900

    :))

--- src/main/java/org/hyunjooon/communication_devtools/domain/account/user/User.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/account/user/User.java
@@ -26,25 +26,25 @@ import java.util.Objects;
 @Entity @Table(name = "users") @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
-    @Id @Comment("사용자 ID")
+    @Id @Comment(value = "사용자 ID")
     private String userId; // 사용자 고유 ID
 
-    @Column(nullable = false) @Comment("사용자 이름") private String userName; // 사용자 이름
-    @Column(nullable = false, unique = true) @Comment("사용자 이메일") private String email; // 사용자 이메일
-    @Column(nullable = false) @Comment("사용자 전화번호") private String phoneNumber; // 사용자 전화번호
-    @Column(nullable = false) @Comment("사용자 나이") @Min(value = 14, message = "나이는 14세 이상이여야 합니다") private int age; // 사용자 나이
+    @Column(nullable = false) @Comment(value = "사용자 이름") private String userName; // 사용자 이름
+    @Column(nullable = false, unique = true) @Comment(value = "사용자 이메일") private String email; // 사용자 이메일
+    @Column(nullable = false) @Comment(value = "사용자 전화번호") private String phoneNumber; // 사용자 전화번호
+    @Column(nullable = false) @Comment(value = "사용자 나이") @Min(value = 14, message = "나이는 14세 이상이여야 합니다") private int age; // 사용자 나이
 
     @Column(nullable = false) @Enumerated(value = EnumType.STRING) @Comment("사용자 성별") private Gender gender; // 사용자 성별
-    @Comment("사용자 깃허브 ID") private String githubId;
-    @Comment("사용자 비밀번호") @Column(nullable = false) private String password; // 사용자 비밀번호
+    @Comment(value = "사용자 깃허브 ID") private String githubId;
+    @Comment(value = "사용자 비밀번호") @Column(nullable = false) private String password; // 사용자 비밀번호
 
-    @Comment("사용자 Oauth Provider") private String provider; // 사용자 Oauth provider
-    @Comment("사용자 Oauth providerId") private String providerId; // 사용자 Oauth providerId
+    @Comment(value = "사용자 Oauth Provider") private String provider; // 사용자 Oauth provider
+    @Comment(value = "사용자 Oauth providerId") private String providerId; // 사용자 Oauth providerId
 
     @Enumerated(value = EnumType.STRING) @Comment("사용자 상태") private Status status; // 사용자 상태(ONLINE: 접속, OFFLINE: 접속해제, DO_NOT_DISTURBED: 방해금지)
 
-    @Comment("사용자 프로필 이미지 이름") private String profileImageName; // 사용자 프로필 이미지 이름
-    @Comment("사용자 설명") private String profileDescription; // 사용자 프로필 기타 설명
+    @Comment(value = "사용자 프로필 이미지 이름") private String profileImageName; // 사용자 프로필 이미지 이름
+    @Comment(value = "사용자 설명") private String profileDescription; // 사용자 프로필 기타 설명
 
     @ElementCollection private List<String> interested = new ArrayList<>(); // 사용자 관심사
 
