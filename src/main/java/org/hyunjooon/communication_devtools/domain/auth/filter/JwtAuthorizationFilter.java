a8b4994af355234d0d2d9b0d0a631925839b1b9c
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-05 13:41:00 +0900

    FEAT: JwtAuthorizationFilter 파일 생성

---
+++ src/main/java/org/hyunjooon/communication_devtools/domain/auth/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,25 @@
+package org.hyunjooon.communication_devtools.domain.auth.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        
+    }
+}
