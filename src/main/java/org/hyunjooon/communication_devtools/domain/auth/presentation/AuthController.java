5d82fd2c1f30be5d854d10ddd447466c108acfc8
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-07 20:24:00 +0900

    DELETE: test용 함수 삭제

--- src/main/java/org/hyunjooon/communication_devtools/domain/auth/presentation/AuthController.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/auth/presentation/AuthController.java
@@ -3,12 +3,10 @@ package org.hyunjooon.communication_devtools.domain.auth.presentation;
 import lombok.RequiredArgsConstructor;
 import org.hyunjooon.communication_devtools.domain.auth.presentation.dto.request.SignInRequest;
 import org.hyunjooon.communication_devtools.domain.auth.presentation.dto.request.SignUpRequest;
-import org.hyunjooon.communication_devtools.domain.auth.presentation.dto.response.UserResponse;
 import org.hyunjooon.communication_devtools.domain.account.user.repository.UserRepository;
 import org.hyunjooon.communication_devtools.domain.auth.service.AuthService;
 import org.hyunjooon.communication_devtools.global.common.GlobalResponse;
 import org.hyunjooon.communication_devtools.global.exception.GlobalException;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,17 +19,6 @@ import java.util.stream.Collectors;
 public class AuthController {
     private final UserRepository userRepository;
     private final AuthService authService;
-
-    @QueryMapping
-    public List<UserResponse> findAll() {
-        return userRepository.findAll().stream()
-                .map(user -> UserResponse.builder()
-                        .userId(user.getUserId())
-                        .email(user.getEmail())
-                        .phoneNumber(user.getPhoneNumber())
-                        .build())
-                .collect(Collectors.toList());
-    }
 
     @PostMapping("/signUp")
     public GlobalResponse<?> signUp(@RequestBody SignUpRequest request) throws GlobalException {
