284aab6c569831941c052febf08470566f7b54a2
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-16 10:21:22 +0900

    FIX: Status enum

--- src/main/java/org/hyunjooon/communication_devtools/domain/account/user/enums/Status.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/account/user/enums/Status.java
@@ -1,7 +1,28 @@
 package org.hyunjooon.communication_devtools.domain.account.user.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Getter
+@AllArgsConstructor
 public enum Status {
-    ONLINE, // 접속
-    OFFLINE, // 미접속
-    DO_NOT_DISTURBED, // 방해금지 모드
+    ONLINE("온라인"),
+    OFFLINE("오프라인"),
+    DO_NOT_DISTURBED("방해금지");
+
+    private final String name;
+    private static final Map<String, Status> descriptions = Collections.unmodifiableMap(
+            Stream.of(values())
+                    .collect(Collectors.toMap(Status::getName, Function.identity()))
+    );
+    public static Status find(String name) {
+        return Optional.ofNullable(descriptions.get(name)).orElse(OFFLINE);
+    }
 }
