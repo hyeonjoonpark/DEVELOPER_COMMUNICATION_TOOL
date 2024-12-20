4264c409cf3e5492dbae8d7e66de42da0acfd572
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-20 10:48:26 +0900

    FEAT: JwtUtil
    jwtUtil에 토큰 검증 로직을 추가하였습니다

--- src/main/java/org/hyunjooon/communication_devtools/global/utils/JwtUtil.java
+++ src/main/java/org/hyunjooon/communication_devtools/global/utils/JwtUtil.java
@@ -4,6 +4,12 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -13,9 +19,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class JwtUtil {
+    private final UserDetailsService userDetailsService;
 
-    public String createToken(String email, Long expiration, String JWT_SECRET) {
+    @Value("${jwt.secret}")
+    private String JWT_SECRET;
+
+    public String createToken(String email, Long expiration) {
         Map<String, Object> headers = new HashMap<>();
         Claims claims = Jwts.claims();
 
@@ -23,14 +34,26 @@ public class JwtUtil {
         headers.put("alg", "HS256");
         claims.put("email", email);
 
-        SecretKey secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(JWT_SECRET)); // JWT_SECRET_KEY를 Base64 decoding
+        SecretKey secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(JWT_SECRET));
 
-        return Jwts.builder()
-                .setHeader(headers)
-                .setClaims(claims)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(secretKey, SignatureAlgorithm.HS256)
-                .compact();
+        return Jwts.builder().setHeader(headers).setClaims(claims).setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(new Date(System.currentTimeMillis() + expiration)).signWith(secretKey, SignatureAlgorithm.HS256).compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(Base64.getDecoder().decode(JWT_SECRET))).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(Base64.getDecoder().decode(JWT_SECRET))).build().parseClaimsJws(token).getBody();
+
+        String email = claims.get("email", String.class);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+        return new UsernamePasswordAuthenticationToken(userDetails, token, userDetails.getAuthorities());
     }
 }
