552bd3e380d7e5017986d66b5d9640df858582e8
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-23 09:44:30 +0900

    FIX: base entity

---
+++ src/main/java/org/hyunjooon/communication_devtools/global/common/BaseTime.java
@@ -0,0 +1,22 @@
+package org.hyunjooon.communication_devtools.global.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTime {
+    @CreatedDate private LocalDateTime createdAt;
+    @LastModifiedDate private LocalDateTime updatedAt;
+    @CreatedBy String createdBy;
+    @LastModifiedBy String updatedBy;
+}
