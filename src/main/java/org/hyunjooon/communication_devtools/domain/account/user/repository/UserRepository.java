a42407b7a97f7fbfe39a870f622cf6f261b2e660
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-27 09:19:00 +0900

    FIX: findByEmail optional

--- src/main/java/org/hyunjooon/communication_devtools/domain/account/user/repository/UserRepository.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/account/user/repository/UserRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     List<User> findAll();
-    User findByEmail(String email);
     User findByUserName(String username);
+    Optional<User> findByEmail(String email);
 }
