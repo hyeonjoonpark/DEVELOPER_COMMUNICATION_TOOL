7f817889f3673ab219617f46286dad7048d2b611
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-27 09:18:43 +0900

    Create OAuthAttributes

---
+++ src/main/java/org/hyunjooon/communication_devtools/domain/auth/oauth/dto/OAuthAttributes.java
@@ -0,0 +1,42 @@
+package org.hyunjooon.communication_devtools.domain.auth.oauth.dto;
+
+import lombok.Builder;
+import org.hyunjooon.communication_devtools.domain.account.user.User;
+import org.hyunjooon.communication_devtools.domain.account.user.enums.Role;
+
+import java.util.Map;
+
+@Builder
+public record OAuthAttributes(
+        Map<String, Object> attributes,
+        String nameAttributeKey,
+        String name,
+        String email,
+        String picture
+) {
+    public static OAuthAttributes of(String registrationId,
+                                     String userNameAttributeName,
+                                     Map<String, Object> attributes) {
+        return ofGithub(userNameAttributeName, attributes);
+    }
+    private static OAuthAttributes ofGithub(String userNameAttributeName,
+                                            Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .userName(name)
+                .email(email)
+                .profileImageName(picture)
+                .role(Role.ROLE_USER)
+                .build();
+    }
+
+}
