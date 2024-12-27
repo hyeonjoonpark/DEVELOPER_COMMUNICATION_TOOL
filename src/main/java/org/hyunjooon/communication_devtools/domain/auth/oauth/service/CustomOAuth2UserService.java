4eec6b69c7ebd236f76a2906a12eb7c2452121e9
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-27 09:18:32 +0900

    FIX: CustomOAuth2UserService

--- src/main/java/org/hyunjooon/communication_devtools/domain/auth/oauth/service/CustomOAuth2UserService.java
+++ src/main/java/org/hyunjooon/communication_devtools/domain/auth/oauth/service/CustomOAuth2UserService.java
@@ -1,20 +1,55 @@
 package org.hyunjooon.communication_devtools.domain.auth.oauth.service;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.hyunjooon.communication_devtools.domain.account.user.User;
 import org.hyunjooon.communication_devtools.domain.account.user.repository.UserRepository;
+import org.hyunjooon.communication_devtools.domain.auth.oauth.dto.OAuthAttributes;
+import org.hyunjooon.communication_devtools.domain.auth.oauth.dto.SessionUser;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final UserRepository userRepository;
+    private final HttpSession httpSession;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        return null;
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        User user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRole().getName())), attributes.attributes(), attributes.nameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = attributes.toEntity();
+        String email = user.getEmail();
+
+        return userRepository.findByEmail(email)
+                .map(
+                        isExist -> isExist.update(
+                                user.getUserName(),
+                                user.getProfileImageName()
+                        )
+                ) // 기존 사용자 업데이트
+                .orElse(user); // 새 사용자 생성
     }
 }
